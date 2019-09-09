fed0f21f6a5418316250125dc6cac89607e1872c
RogerioNascimento-dev <rogerionascimento.dev@outlook.com.br>
2019-09-08 22:57:49 -0300

    método de cadastro de usuário implementado

--- migrations/20190908211819_create_table_users.js
+++ migrations/20190908211819_create_table_users.js
@@ -2,8 +2,8 @@
 exports.up = function(knex, Promise) {
   return knex.schema.createTable('users', table =>{
       table.increments('id').primary();
-      table.string('mome').notNull();
-      table.string('email').notNull();
+      table.string('name').notNull();
+      table.string('email').notNull().unique();
       table.string('password').notNull();
   })
 };
