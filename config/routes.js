ee36e2580581ae768632f09a022c750721ba04e1
RogerioNascimento-dev <rogerionascimento.dev@outlook.com.br>
2019-09-11 23:22:57 -0300

    adicinado rota /

--- config/routes.js
+++ config/routes.js
@@ -2,6 +2,8 @@ module.exports = app =>{
     app.post('/signup', app.api.user.save)
     app.post('/signin', app.api.auth.signin)
 
+    app.get('/', app.api.home.servicesHome)
+
     app.route('/tasks')
         .all(app.config.passport.authenticate())
         .get(app.api.task.getTasks)
