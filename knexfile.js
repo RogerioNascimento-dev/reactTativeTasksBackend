4e1960abbe822ef91ffa62deb265b2a66eb6c138
RogerioNascimento-dev <rogerionascimento.dev@outlook.com.br>
2019-09-12 23:27:46 -0300

    adicionando o host nas configurações do knex

--- knexfile.js
+++ knexfile.js
@@ -3,6 +3,7 @@ require('dotenv/config');
 module.exports = {
     client: 'postgresql',
     connection: {
+      host: process.env.DATABASE_HOST,
       database: process.env.DATABASE,
       user:     process.env.USER_DATABASE,
       password: process.env.PASSWORD_DATABASE,
